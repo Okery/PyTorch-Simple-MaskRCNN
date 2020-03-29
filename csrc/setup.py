2e5ce0c74a53e71cd690b726f0480c5810c8ac67
Xin Wu <47376448+Jaramies@users.noreply.github.com>
2020-03-29 22:09:38 +0800

    Delete setup.py

--- csrc/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-
-setup(name='MaskRCNN Extension',
-      ext_modules=[CUDAExtension('_C', ['nms.cpp', 'nms_cpu.cpp', 'nms_cuda.cu'])],
-      cmdclass={'build_ext': BuildExtension})
