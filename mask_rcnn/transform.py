00749438ad92802d308a32ed035f16073d799419
Xin Wu <47376448+Okery@users.noreply.github.com>
2020-03-31 15:15:55 +0800

    Add files via upload

--- mask_rcnn/transform.py
+++ mask_rcnn/transform.py
@@ -5,12 +5,11 @@ import torch.nn.functional as F
 
 
 class Transformer:
-    def __init__(self, min_size, max_size, image_mean, image_std, mask_threshold):
+    def __init__(self, min_size, max_size, image_mean, image_std):
         self.min_size = min_size
         self.max_size = max_size
         self.image_mean = image_mean
         self.image_std = image_std
-        self.mask_threshold = mask_threshold
         
     def __call__(self, image, target):
         image = self.normalize(image)
@@ -37,8 +36,8 @@ class Transformer:
             return image, target
         
         box = target['boxes']
-        box[:, [0, 2]] = box[:, [0, 2]] * image.shape[-2] / ori_image_shape[0]
-        box[:, [1, 3]] = box[:, [1, 3]] * image.shape[-1] / ori_image_shape[1]
+        box[:, [0, 2]] = box[:, [0, 2]] * image.shape[-1] / ori_image_shape[1]
+        box[:, [1, 3]] = box[:, [1, 3]] * image.shape[-2] / ori_image_shape[0]
         target['boxes'] = box
         
         if 'masks' in target:
@@ -60,14 +59,13 @@ class Transformer:
     
     def postprocess(self, result, image_shape, ori_image_shape):
         box = result['boxes']
-        box[:, [0, 2]] = box[:, [0, 2]] * ori_image_shape[0] / image_shape[0]
-        box[:, [1, 3]] = box[:, [1, 3]] * ori_image_shape[1] / image_shape[1]
+        box[:, [0, 2]] = box[:, [0, 2]] * ori_image_shape[1] / image_shape[1]
+        box[:, [1, 3]] = box[:, [1, 3]] * ori_image_shape[0] / image_shape[0]
         result['boxes'] = box
         
         if 'masks' in result:
             mask = result['masks']
             mask = paste_masks_in_image(mask, box, 1, ori_image_shape)
-            mask = (mask >= self.mask_threshold).byte()
             result['masks'] = mask
             
         return result
@@ -78,19 +76,19 @@ def expand_detection(mask, box, padding):
     scale = (M + 2 * padding) / M
     padded_mask = torch.nn.functional.pad(mask, (padding,) * 4)
     
-    h_half = (box[:, 2] - box[:, 0]) * 0.5
-    w_half = (box[:, 3] - box[:, 1]) * 0.5
-    y_c = (box[:, 2] + box[:, 0]) * 0.5
-    x_c = (box[:, 3] + box[:, 1]) * 0.5
+    w_half = (box[:, 2] - box[:, 0]) * 0.5
+    h_half = (box[:, 3] - box[:, 1]) * 0.5
+    x_c = (box[:, 2] + box[:, 0]) * 0.5
+    y_c = (box[:, 3] + box[:, 1]) * 0.5
 
-    h_half = h_half * scale
     w_half = w_half * scale
+    h_half = h_half * scale
 
     box_exp = torch.zeros_like(box)
-    box_exp[:, 0] = y_c - h_half
-    box_exp[:, 2] = y_c + h_half
-    box_exp[:, 1] = x_c - w_half
-    box_exp[:, 3] = x_c + w_half
+    box_exp[:, 0] = x_c - w_half
+    box_exp[:, 2] = x_c + w_half
+    box_exp[:, 1] = y_c - h_half
+    box_exp[:, 3] = y_c + h_half
     return padded_mask, box_exp.to(torch.int64)
 
 
@@ -102,15 +100,15 @@ def paste_masks_in_image(mask, box, padding, image_shape):
     im_mask = torch.zeros(size, dtype=mask.dtype, device=mask.device)
     for m, b, im in zip(mask, box, im_mask):
         b = b.tolist()
-        h = max(b[2] - b[0], 1)
-        w = max(b[3] - b[1], 1)
+        w = max(b[2] - b[0], 1)
+        h = max(b[3] - b[1], 1)
         
         m = F.interpolate(m[None, None], size=(h, w), mode='bilinear', align_corners=False)[0][0]
 
-        y1 = max(b[0], 0)
-        x1 = max(b[1], 0)
-        y2 = min(b[2], image_shape[0])
-        x2 = min(b[3], image_shape[1])
+        x1 = max(b[0], 0)
+        y1 = max(b[1], 0)
+        x2 = min(b[2], image_shape[1])
+        y2 = min(b[3], image_shape[0])
 
-        im[y1:y2, x1:x2] = m[(y1 - b[0]):(y2 - b[0]), (x1 - b[1]):(x2 - b[1])]
+        im[y1:y2, x1:x2] = m[(y1 - b[1]):(y2 - b[1]), (x1 - b[0]):(x2 - b[0])]
     return im_mask
