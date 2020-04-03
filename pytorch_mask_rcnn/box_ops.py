5fc55fb7eeb247a05a6d30c6652da040340ec9aa
Xin Wu <47376448+Okery@users.noreply.github.com>
2020-04-03 16:25:15 +0800

    Add files via upload

---
+++ pytorch_mask_rcnn/box_ops.py
@@ -0,0 +1,95 @@
+import math
+
+import torch
+
+
+class BoxCoder:
+    def __init__(self, weights, bbox_xform_clip=math.log(1000. / 16)):
+        self.weights = weights
+        self.bbox_xform_clip = bbox_xform_clip
+
+    def encode(self, reference_box, proposal):
+        width = proposal[:, 2] - proposal[:, 0]
+        height = proposal[:, 3] - proposal[:, 1]
+        ctr_x = proposal[:, 0] + 0.5 * width
+        ctr_y = proposal[:, 1] + 0.5 * height
+
+        gt_width = reference_box[:, 2] - reference_box[:, 0]
+        gt_height = reference_box[:, 3] - reference_box[:, 1]
+        gt_ctr_x = reference_box[:, 0] + 0.5 * gt_width
+        gt_ctr_y = reference_box[:, 1] + 0.5 * gt_height
+
+        dx = self.weights[0] * (gt_ctr_x - ctr_x) / width
+        dy = self.weights[1] * (gt_ctr_y - ctr_y) / height
+        dw = self.weights[2] * torch.log(gt_width / width)
+        dh = self.weights[3] * torch.log(gt_height / height)
+
+        delta = torch.stack((dx, dy, dw, dh), dim=1)
+        return delta
+
+    def decode(self, delta, box):
+        dx = delta[:, 0] / self.weights[0]
+        dy = delta[:, 1] / self.weights[1]
+        dw = delta[:, 2] / self.weights[2]
+        dh = delta[:, 3] / self.weights[3]
+
+        dw = torch.clamp(dw, max=self.bbox_xform_clip)
+        dh = torch.clamp(dh, max=self.bbox_xform_clip)
+
+        width = box[:, 2] - box[:, 0]
+        height = box[:, 3] - box[:, 1]
+        ctr_x = box[:, 0] + 0.5 * width
+        ctr_y = box[:, 1] + 0.5 * height
+
+        pred_ctr_x = dx * width + ctr_x
+        pred_ctr_y = dy * height + ctr_y
+        pred_w = torch.exp(dw) * width
+        pred_h = torch.exp(dh) * height
+
+        xmin = pred_ctr_x - 0.5 * pred_w
+        ymin = pred_ctr_y - 0.5 * pred_h
+        xmax = pred_ctr_x + 0.5 * pred_w
+        ymax = pred_ctr_y + 0.5 * pred_h
+
+        target = torch.stack((xmin, ymin, xmax, ymax), dim=1)
+        return target
+
+    
+def box_iou(box_a, box_b):
+    lt = torch.max(box_a[:, None, :2], box_b[:, :2])
+    rb = torch.min(box_a[:, None, 2:], box_b[:, 2:])
+
+    wh = (rb - lt).clamp(min=0)
+    inter = wh[:, :, 0] * wh[:, :, 1]
+    area_a = torch.prod(box_a[:, 2:] - box_a[:, :2], 1)
+    area_b = torch.prod(box_b[:, 2:] - box_b[:, :2], 1)
+    
+    return inter / (area_a[:, None] + area_b - inter)
+
+
+def process_box(box, score, image_shape, min_size):
+    box[:, [0, 2]] = box[:, [0, 2]].clamp(0, image_shape[1]) 
+    box[:, [1, 3]] = box[:, [1, 3]].clamp(0, image_shape[0]) 
+
+    w, h = box[:, 2] - box[:, 0], box[:, 3] - box[:, 1]
+    keep = torch.where((w >= min_size) & (h >= min_size))[0]
+    box, score = box[keep], score[keep]
+    return box, score
+
+
+def nms(box, score, threshold):
+    return torch.ops.torchvision.nms(box, score, threshold)
+    
+
+# just for test. It is too slow. Don't use it during train
+def slow_nms(box, nms_thresh):
+    idx = torch.arange(box.size(0))
+    
+    keep = []
+    while idx.size(0) > 0:
+        keep.append(idx[0].item())
+        head_box = box[idx[0], None, :]
+        remain = torch.where(box_iou(head_box, box[idx]) <= nms_thresh)[1]
+        idx = idx[remain]
+    
+    return keep
