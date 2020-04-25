b72fdc35649fd2514624da696ded3677b408cc12
Xin Wu <47376448+Okery@users.noreply.github.com>
2020-04-25 14:28:38 +0800

    Add files via upload

--- pytorch_mask_rcnn/__init__.py
+++ pytorch_mask_rcnn/__init__.py
@@ -1,4 +1,7 @@
 from .mask_rcnn import maskrcnn_resnet50
 from .datasets import datasets
 from .test import show, APGetter
-from .evaluate import CocoEvaluator
+try:
+    from .coco_eval import CocoEvaluator
+except:
+    pass
