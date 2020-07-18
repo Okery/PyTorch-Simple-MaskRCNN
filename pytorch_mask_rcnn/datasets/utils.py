912888a99b8549aa63a5787566f6b1a5db4dade1
Xin Wu <47376448+Okery@users.noreply.github.com>
2020-07-18 14:26:48 +0800

    Add files via upload

--- pytorch_mask_rcnn/datasets/utils.py
+++ pytorch_mask_rcnn/datasets/utils.py
@@ -1,9 +1,7 @@
-import torch
-
 from .voc_dataset import VOCDataset
 from .coco_dataset import COCODataset
 
-__all__ = ["datasets", "collate_wrapper", "DataPrefetcher"]
+__all__ = ["datasets", "collate_wrapper"]
 
 
 def datasets(ds, *args, **kwargs):
@@ -32,32 +30,4 @@ class CustomBatch:
         self.images = [img.pin_memory() for img in self.images]
         self.targets = [{k: v.pin_memory() for k, v in tgt.items()} for tgt in self.targets]
         return self
-    
-
-class DataPrefetcher:
-    def __init__(self, data_loader):
-        self.loader = data_loader
-        self.dataset = data_loader.dataset
-        self.stream = torch.cuda.Stream()
-        
-    def __iter__(self):
-        for i, d in enumerate(self.loader, 1):
-            if i == 1:
-                d.images = [img.cuda(non_blocking=True) for img in d.images]
-                d.targets = [{k: v.cuda(non_blocking=True) for k, v in tgt.items()} for tgt in d.targets]
-                self._cache = d
-                continue
-               
-            torch.cuda.current_stream().wait_stream(self.stream)
-            out = self._cache
-            
-            with torch.cuda.stream(self.stream):
-                d.images = [img.cuda(non_blocking=True) for img in d.images]
-                d.targets = [{k: v.cuda(non_blocking=True) for k, v in tgt.items()} for tgt in d.targets]
-            self._cache = d
-            yield out
-        yield self._cache
-        
-    def __len__(self):
-        return len(self.loader)
     
